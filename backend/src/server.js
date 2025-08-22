5ec14ce092dd9f35606e936f55e7c1dd179cf49d
Joseeul <jose_andreas@icloud.com>
2025-08-22 13:01:37 +0700

    Database schema

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,17 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
+
+connectDB();
+
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
-  console.log("Server run on port: 5001");
+app.listen(PORT, () => {
+  console.log("Server run on port:", PORT);
 });
